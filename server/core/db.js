f345dacde423c894aba70d8ef3e330a686f04d71
Don Kendall <kendall@donkendall.com>
2022-07-05 20:32:55 -0400

    imp: allow selfsigned ssl without ca

--- server/core/db.js
+++ server/core/db.js
@@ -80,7 +80,7 @@ module.exports = {
         dbClient = 'pg'
 
         if (dbUseSSL && _.isPlainObject(dbConfig)) {
-          dbConfig.ssl = (sslOptions === true) ? { rejectUnauthorized: true } : sslOptions
+          dbConfig.ssl = (sslOptions === true) ? { rejectUnauthorized: false } : sslOptions
         }
         break
       case 'mariadb':
